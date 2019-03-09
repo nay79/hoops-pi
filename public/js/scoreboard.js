84cebabcb378f7fb6b1783b982aabaf7f0e7d388
Patrick Catanzariti <pat@devdiner.com>
2019-03-09 11:27:05 +1100

    The glorious example that actually works

---
+++ public/js/scoreboard.js
@@ -0,0 +1,24 @@
+(function() {
+    var ws = new WebSocket('ws://192.168.1.133:8080', 'json');
+    var score = 0;
+
+    ws.onopen = function () {
+        console.log('Websocket is open');
+        ws.send('Hi from React.');
+        document.getElementById('score').innerHTML = score;
+    };
+
+    ws.onmessage = function (event) {
+        if (event.data.indexOf('SCORE:') != -1) {
+            score++;
+            document.getElementById('score').innerHTML = score;
+        }
+        console.log('Message was ', event.data);
+        //document.getElementById('score').innerHTML = event.data;
+    };
+
+    ws.onerror = function(error) {
+       console.log('Error detected: ' + error.data);
+    }
+
+}());
